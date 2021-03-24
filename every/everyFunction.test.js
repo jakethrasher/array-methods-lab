73ae54a03a9aefe47f1559eefab1301c83594ef4
Jake Thrasher <mjakethrasher@gmail.com>
2021-03-23 19:41:31 -0700

    refactor

--- every/everyFunction.test.js
+++ every/everyFunction.test.js
@@ -1,19 +1,16 @@
 const everyFunction = require('./everyFunction');
 
 describe('everyFunction', ()=> {
-    
-    const booleanArray = [true, true, false]
-
-    it('returns a boolean value if all elements of an array meet the requirements of the callback', ()=>{
-
+    it('returns a boolean value based on whether or not all elements of an array meet the requirements of the callback', ()=>{
+        
+        const booleanArray = [true, true, false]
         const trueOrFalse = (boolean)=> {
-
-            if(boolean){
-                return true;
-            }
+            if(boolean) return true;
             else return false;
         }
-    
         expect(everyFunction(booleanArray, trueOrFalse)).toBeFalsy();
     })
 });
+    
+    
+    
